b59238c629e59a007572845553e6b4b81a60a227
EthanAmato <ethannamato@gmail.com>
2024-02-29 14:56:48 -0500

    added form with partial validation

---
+++ src/components/ui/form/SpreadsheetDialog.tsx
@@ -0,0 +1,181 @@
+"use client";
+import {
+  Dispatch,
+  SetStateAction,
+  useState,
+  useEffect,
+  FormEvent,
+} from "react";
+import { z } from "zod";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "../button";
+
+type SpreadsheetDialogProps = {
+  file: File;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+type Axes = {
+  x: string;
+  y: string;
+  z: string;
+};
+type Axis = keyof Axes;
+
+// Import necessary hooks and components
+import readXlsxFile from "read-excel-file";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../select";
+import { Input } from "../input";
+import { Controller, Form, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const dialogFormSchema = z.object({
+  title: z.string().min(1, "Graph title is required"),
+  x: z.string().min(1, "X axis selection is required"),
+  y: z.string().min(1, "Y axis selection is required"),
+  z: z.string().min(1, "Z axis selection is required"),
+  colorBy: z.string().optional(), // Make 'color by' optional
+});
+
+const SpreadsheetDialog = ({
+  file,
+  isOpen,
+  setIsOpen,
+}: SpreadsheetDialogProps) => {
+  const [columnNames, setColumnNames] = useState<string[]>([]);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(dialogFormSchema),
+  });
+
+  useEffect(() => {
+    readXlsxFile(file).then((rows) => {
+      const headers = rows[0] as string[];
+      setColumnNames(headers);
+    });
+  }, [file]);
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    setIsOpen(false);
+    // Handle the submission logic here
+  };
+  return (
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Customize your Graph!</AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogDescription asChild>
+          <>
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+              <div>
+                <Controller
+                  name="title"
+                  control={control}
+                  render={({ field }) => (
+                    <input {...field} placeholder="Enter graph title" />
+                  )}
+                />
+                {/* Check if the error message is defined before rendering */}
+                {/* {errors.title?.message && (
+                  <p className="text-red-600">{errors.title.message}</p>
+                )} */}
+              </div>
+
+              <div>
+                {(["x", "y", "z"] as const).map((axis) => (
+                  <div key={axis}>
+                    <Controller
+                      name={axis}
+                      control={control}
+                      render={({ field }) => (
+                        <>
+                          <Select {...field} onValueChange={field.onChange}>
+                            <SelectTrigger>
+                              <SelectValue
+                                placeholder={`Select ${axis.toUpperCase()} axis`}
+                              />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectGroup>
+                                <SelectLabel>Columns</SelectLabel>
+                                {columnNames.map((name, index) => (
+                                  <SelectItem key={index} value={name}>
+                                    {name}
+                                  </SelectItem>
+                                ))}
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
+                          {/* Display the error message if there's an error for this field */}
+                          {/* {errors[axis] && (
+                            <p className="text-red-600">
+                              {errors[axis]?.message}
+                            </p>
+                          )} */}
+                        </>
+                      )}
+                    />
+                  </div>
+                ))}
+              </div>
+
+              <Controller
+                name="colorBy"
+                control={control}
+                render={({ field }) => (
+                  <Select {...field} onValueChange={field.onChange}>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select column to color by" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Columns</SelectLabel>
+                        {columnNames.map((name, index) => (
+                          <SelectItem key={index} value={name}>
+                            {name}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+
+              <Button type="submit">Submit</Button>
+            </form>
+          </>
+        </AlertDialogDescription>
+        <AlertDialogFooter>
+          <Button onClick={() => setIsOpen(false)}>Close</Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default SpreadsheetDialog;
---
+++ src/components/ui/form/SpreadsheetForm.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+import SpreadsheetDialog from "./SpreadsheetDialog";
+
+const ACCEPTED_IMAGE_TYPES = [
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+];
+const formSchema = z.object({
+  file: z
+    .instanceof(File)
+    .refine(
+      (file) => ACCEPTED_IMAGE_TYPES.includes(file.type),
+      "Only .xlsx files are allowed."
+    ),
+});
+export function SpreadSheetForm() {
+  const [isFormSubmitted, setIsFormSubmitted] = useState<boolean>(false);
+  const [spreadsheetFile, setSpreadsheetFile] = useState<File | null>(null);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(value: z.infer<typeof formSchema>) {
+    setIsFormSubmitted(true);
+    setSpreadsheetFile(value.file);
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="file"
+            render={({ field: { onChange, ...field } }) => (
+              <FormItem>
+                <FormLabel>Spreadsheet</FormLabel>
+                <FormControl>
+                  {/* Manually handle the onChange event */}
+                  <Input
+                    type="file"
+                    onChange={(e) => {
+                      const files = e.target.files;
+                      const file = files && files[0] ? files[0] : null;
+                      if (file) {
+                        onChange(file); // Update the form state with the single file
+                      }
+                    }}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Please upload your .XLSX file...
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+      {isFormSubmitted && (
+        <SpreadsheetDialog
+          file={spreadsheetFile!}
+          isOpen={isFormSubmitted}
+          setIsOpen={setIsFormSubmitted}
+        />
+      )}
+    </>
+  );
+}
